3aec7503d7f3009eadb690af90e738beef48b4c3
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-11 02:15:42 +0600

    function recall

---
+++ function.cpp
@@ -0,0 +1,13 @@
+// This is function recall systems.
+
+#include<iostream>
+using namespace std;
+void add(int a, int b)   // This is formal parameters.
+{
+    int sum=a+b;
+    cout<<sum;
+}
+int main(){
+    int m,n;
+    add(m,n);  // This is actual parameters
+}
