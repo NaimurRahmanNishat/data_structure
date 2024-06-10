aae86131fd3f63e3634e7f2da5c5d27f38c5d905
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-11 04:03:15 +0600

    call by value

--- function.cpp
+++ function.cpp
@@ -1,13 +1,27 @@
 // This is function recall systems.
 
+// #include<iostream>
+// using namespace std;
+// void add(int a, int b)   // This is formal parameters.
+// {
+//     int sum=a+b;
+//     cout<<sum;
+// }
+// int main(){
+//     int m,n;
+//     add(m,n);  // This is actual parameters
+// }
+
+
+// This is call by value systems.
 #include<iostream>
 using namespace std;
-void add(int a, int b)   // This is formal parameters.
+void add(int x, int y)  
 {
-    int sum=a+b;
-    cout<<sum;
+    x=20, y=10;
 }
 int main(){
-    int m,n;
-    add(m,n);  // This is actual parameters
+    int x=10, y=20;
+    add(x,y); 
+    cout<<x << " " << y;
 }
