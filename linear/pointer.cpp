f749a0b939b3941aa7eb941bbbc5d5ba4dd3f8bc
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-10 22:55:45 +0600

    pass value pointer

--- linear/pointer.cpp
+++ linear/pointer.cpp
@@ -1,8 +1,23 @@
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     int x=10;
+//     int *px = &x;
+//     cout<<*px <<endl <<&x;  // *px print the value and &x print the memory location.
+//     return 0;
+// }
+
+// pass by value of pointer.
+
 #include<iostream>
 using namespace std;
-int main (){
+void change(int x)
+{
+    x=20; 
+    cout<<x <<endl;
+}
+int main(){
     int x=10;
-    int *px = &x;
-    cout<<*px <<endl <<&x;  // *px print the value and &x print the memory location.
-    return 0;
+    change(x);
+    cout<<x;
 }
