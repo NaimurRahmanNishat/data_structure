c0bdd3fe41fa9c833db1ba50524f360ce897e1b4
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-12 13:00:47 +0600

    adding linklist

---
+++ linear/linklist.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* link;
+};
+
+struct Node* head;
+
+void print(){
+    struct Node* temp;
+    temp = head;
+    while (temp != NULL)
+    {
+        cout<<temp->data << " ";
+        temp= temp->link;
+    }
+    
+}
+
+int main(){
+    head=NULL;
+    head = (struct Node*) malloc(sizeof(struct Node));
+    head->data=2;
+    head->link = (struct Node*) malloc(sizeof(struct Node));
+    head->link->data=5;
+    head->link->link=NULL;
+    print();
+    return 0;
+}
