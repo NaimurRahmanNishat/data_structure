e2739d5600f89c8da7b3cff0dc313f6e10a0b51e
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-13 02:15:59 +0600

    add threetype function

--- function.cpp
+++ function.cpp
@@ -1,31 +1,30 @@
 // This is function recall systems.
 
-// #include<iostream>
-// using namespace std;
-// void add(int a, int b)   // This is formal parameters.
-// {
-//     int sum=a+b;
-//     cout<<sum;
-// }
-// int main(){
-//     int m,n;
-//     add(m,n);  // This is actual parameters
-// }
+#include<iostream>
+using namespace std;
+void add(int a, int b)   // This is formal parameters.
+{
+    int sum=a+b;
+    cout<<sum;
+}
+int main(){
+    int m,n;
+    add(m,n);  // This is actual parameters
+}
 
 
 // This is call by value systems.
-// #include<iostream>
-// using namespace std;
-// void add(int x, int y)  
-// {
-//     x=20, y=10;
-// }
-// int main(){
-//     int x=10, y=20;
-//     add(x,y); 
-//     cout<<x << " " << y;
-// }
-
+#include<iostream>
+using namespace std;
+void add(int x, int y)  
+{
+    x=20, y=10;
+}
+int main(){
+    int x=10, y=20;
+    add(x,y); 
+    cout<<x << " " << y;
+}
 
 
 #include<iostream>
@@ -87,9 +86,33 @@ std::pair<int, float> addition(int a, int b){
     cout<< name << endl;
     return make_pair(sum,sub); // return sum instead of addition
 }
-
 int main(){
     int a = 10, b = 5; // Correct the variable declaration
     std::pair<int, float> result = addition(a, b);
     return 0; // Return 0 to indicate successful completion
+}
+
+#include<iostream>
+using namespace std;
+int add(int a, int b){
+    int sum=a+b;
+    return sum;
+}
+float sub(float x, float y){
+    float sub_result=x-y;
+    return sub_result;
+}
+void result(string a){
+    string add=a;
+    cout<<a;
+}
+int main(){
+    int a=5,b=7;
+    int sum_add =add(a,b);
+    cout<<sum_add << endl;
+    float x=6.5,y=3.1;
+    float sub_function=sub(x,y);
+    cout<<sub_function << endl;
+    string name= "Naimur Rahman";
+    result(name);
 }
