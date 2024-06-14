3a42838173b2812ff676870ffb4b83ba15001c18
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-14 15:16:10 +0600

    linklist added comment

--- linear/linklist/linklist.cpp
+++ linear/linklist/linklist.cpp
@@ -1,31 +1,32 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
-struct Node
+struct Node  //This is different block (node) create and store value. A node has two part one is data part and other one data/link part.The second node connect the first link part.
 {
-    int data;
-    struct Node* link;
+    int data;  // This is node data.
+    struct Node* link;   // This is Node link. This link connect the another node.
 };
 
-struct Node* head;
+struct Node* head;   // This is global variable diclare.
 
-void print(){
-    struct Node* temp;
-    temp = head;
-    while (temp != NULL)
+void print(){    // create a function. This function name is print.
+    struct Node* temp;   // declare a vaiable. This is node type variable and this variable value is temp.
+    temp = head;   // head sotre the temp.
+    while (temp != NULL)  // check the condition.
     {
-        cout<<temp->data << " ";
-        temp= temp->link;
+        cout<<temp->data << " "; // print temp/head data.
+        temp= temp->link;  // This is true again go back the loop and check the condition and print the value.
     }
     
 }
 
-int main(){
-    head=NULL;
-    head = (struct Node*) malloc(sizeof(struct Node));
-    head->data=2;
-    head->link = (struct Node*) malloc(sizeof(struct Node));
-    head->link->data=5;
-    head->link->link=NULL;
-    print();
+int main(){  // main function.
+    head=NULL;  // first of all head (null).
+    head = (struct Node*) malloc(sizeof(struct Node)); // create a node size and store the value.
+    head->data=2;  // A node has tow part one data and another one link. the data value 2.
+    head->link = (struct Node*) malloc(sizeof(struct Node)); // conecting the another node.
+    head->link->data=5; // This is second node value.
+    head->link->link=NULL; // The second node link is null because there has no node.
+    print();  // call the function.
     return 0;
 }
