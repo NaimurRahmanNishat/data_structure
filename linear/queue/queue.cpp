ab5fbc6a0bc9f87c4c0dd3c2e9fe6aa61ba417b9
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-07-09 13:03:49 +0600

    added rename short queue

--- linear/queue/queue.cpp
+++ linear/queue/queue.cpp
@@ -175,61 +175,62 @@ int main(){
 
 
 // Shorts Queue.
-#include <iostream>
-using namespace std;
+#include <iostream>       // This is header file.
+using namespace std;      // This is standared libarary.
 
-class Queue {
-private:
-    int front, rear, n;
-    int *arr;
+class Queue {             // This is a class. Class first name always capitailize.
+private:                  // A class has two part one is private and another one is public.
+    int front, rear, n;   // Initilize a data type.
+    int *arr;             // Initilize a pointer. This is memory location.
 
-public:
-    Queue(int size) {
-        n = size;
-        arr = new int[n];
-        front = rear = -1;
+public:                   // This is public class.
+    Queue(int size) {     // This Queue is constructor. int size is constructor parameter.
+        n = size;         //  This line assigns the value of the constructor parameter size to the member variable n.    
+        arr = new int[n]; // This line dynamically allocates an array of integers of size n on the heap and assigns its address to the pointer arr.
+        front = rear = -1;// This line initializes the front and rear member variables to -1. This typically indicates that the queue is initially empty.
     }
 
-    void enqueue(int value) {
-        if (rear == n - 1) {
-            cout << "Overflow" << endl;
-        } else if (front == -1 && rear == -1) {
-            front = rear = 0;
-            arr[rear] = value;
-        } else {
-            rear++;
-            arr[rear] = value;
+void enqueue(int value) {                     // This is a void function named enqueue. It takes one integer parameter called value. This function is a member of the Queue class.
+    if (rear == n - 1) {                      // This checks if the rear index has reached the maximum size of the queue (n - 1), indicating that the queue is full.
+        cout << "Overflow" << endl;           // If the queue is full, it prints "Overflow" to the console and exits the function.
+    } else if (front == -1 && rear == -1) {   // This checks if the queue is empty by verifying that both front and rear are -1.
+        front = rear = 0;                     // If the queue is empty, it sets both front and rear to 0, indicating that the queue will now have its first element.
+        arr[rear] = value;                    // It assigns the value to the first position in the array (queue).
+    } else {                                  // If the queue is not empty and not full:
+        rear++;                               // It increments the rear index to point to the next empty position in the queue.
+        arr[rear] = value;                    // It assigns the value to the position at the new rear index.
+    }
+};
+
+int dequeue() {                                   // This is an integer function named dequeue. It returns an integer value. This function is a member of the Queue class.
+    if (front == -1 && rear == -1) {              // This checks if both front and rear are -1, indicating that the queue is empty.
+        cout << "Queue is Empty" << endl;         // If the queue is empty, it prints "Queue is Empty" to the console.
+        return -1;                                // It returns -1 to indicate that the dequeue operation failed because the queue is empty.
+    } else if (front == rear) {                   // This checks if front and rear are equal, indicating that the queue has only one element.
+        int value = arr[front];                   // It retrieves the value at the front of the queue.
+        front = rear = -1;                        // It sets both front and rear to -1, indicating that the queue is now empty after removing the element.
+        return value;                             // It returns the retrieved value.
+    } else {                                      // If the queue has more than one element:
+        return arr[front++];                      // It returns the value at the front of the queue and increments the front index to point to the next element.
+    }
+};
+
+void display() {                                   // This is a void function named display. It does not return a value. This function is a member of the Queue class.
+    if (front == -1 && rear == -1) {               // This checks if both front and rear are -1, indicating that the queue is empty.
+        cout << "Queue is Empty" << endl;          // If the queue is empty, it prints "Queue is Empty" to the console.
+    } else {                                       // If the queue is not empty:
+        cout << "Queue elements: ";                // It prints "Queue elements: " to the console as a header for the list of elements.
+        for (int i = front; i <= rear; i++) {      // This is a for loop that starts from the front index and goes up to and including the rear index.
+            cout << arr[i] << " ";                 // Inside the loop, it prints the element at the current index i followed by a space.
         }
+        cout << endl;                              // After the loop, it prints a newline character to move the cursor to the next line in the console.
     }
+};
 
-    int dequeue() {
-        if (front == -1 && rear == -1) {
-            cout << "Queue is Empty" << endl;
-            return -1; // Return a value indicating the queue is empty
-        } else if (front == rear) {
-            int value = arr[front];
-            front = rear = -1;
-            return value;
-        } else {
-            return arr[front++];
-        }
-    }
+~Queue() {                          // This is the destructor for the Queue class. It is called when an object of the Queue class is destroyed.
+    delete[] arr;                   // This deallocates the memory that was previously allocated for the array arr.
+};
 
-    void display() {
-        if (front == -1 && rear == -1) {
-            cout << "Queue is Empty" << endl;
-        } else {
-            cout << "Queue elements: ";
-            for (int i = front; i <= rear; i++) {
-                cout << arr[i] << " ";
-            }
-            cout << endl;
-        }
-    }
-
-    ~Queue() {
-        delete[] arr;
-    }
 };
 
 int main() {
