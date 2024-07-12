bde9b8db5008005b350f9779b79a196c6300056d
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-07-12 10:56:29 +0600

    added pointer update

--- Other_Related/File Pointer & Struct/pointer.cpp
+++ Other_Related/File Pointer & Struct/pointer.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
-int main (){
-    int x=10;
-    int *px = &x;
-    cout<<*px <<endl <<&x;  // *px print the value and &x print the memory location.
+int main(){
+    int x = 5;           // 5 store the x.
+    int *p;              // initilize the pointer.
+    p = &x;              // x address store the pointer(p).
+    cout<< x << endl;    // print the value(5).
+    cout<< *p << endl;   // print the value(5).
+    cout<< p << endl;    // print the memory address.
+    cout<< &x << endl;   // print the memory address.
     return 0;
-}
+};
 
 // pass by value of pointer.
 
