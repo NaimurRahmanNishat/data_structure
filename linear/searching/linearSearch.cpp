2c2e353b8a3b518678be562fc902610083a3e3fc
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-07-25 02:31:52 +0600

    added linear search

---
+++ linear/searching/linearSearch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index of the target element
+        }
+    }
+    return -1; // Return -1 if the target element is not found
+};
+int main() {
+    int arr[] = {2, 4, 0, 1, 9};               // array initilize.
+    int n = sizeof(arr) / sizeof(arr[0]);       // This is array size operation.
+    int target = 9;      // This 9 is searching value.
+    int result = linearSearch(arr, n, target);     // call back the linearSearch.
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found in the array" << endl;
+    }
+    return 0;
+};
